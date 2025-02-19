0eac9c03adb4408a2dd2680929acafdd8bde8eef
Pedro Borges Jr <ppmbjr@gmail.com>
2025-02-19 18:25:53 -0300

    feat: added restaurant category page with components

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        //
+        hostname: "u9a6wmr3as.ufs.sh",
+      },
+      {
+        hostname: "iwritingsolutions.com"
+      }
+    ],
+  },
 };
 
 export default nextConfig;
---
+++ src/app/[slug]/menu/page.tsx
@@ -0,0 +1,45 @@
+import { notFound } from "next/navigation";
+
+import { db } from "@/lib/prisma";
+
+import RestaurantCategories from "./components/RestaurantCategories";
+import RestaurantHeader from "./components/RestaurantHeader";
+
+interface RestaurantMenuPageProps {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ consumptionMethod: string }>;
+}
+
+const isConsumptionMethodValid = (consumptionMethod: string) => {
+  return ["DINE_IN", "TAKEAWAY"].includes(consumptionMethod.toUpperCase());
+};
+
+const RestaurantMenuPage = async ({
+  params,
+  searchParams,
+}: RestaurantMenuPageProps) => {
+  const { slug } = await params;
+  const { consumptionMethod } = await searchParams;
+  if (!isConsumptionMethodValid(consumptionMethod)) {
+    return notFound();
+  }
+  const restaurant = await db.restaurant.findUnique({
+    where: { slug },
+    include: {
+      menuCategories: {
+        include: { products: true },
+      },
+    },
+  });
+  if (!restaurant) {
+    return notFound();
+  }
+  return (
+    <div>
+      <RestaurantHeader restaurant={restaurant} />
+      <RestaurantCategories restaurant={restaurant} />
+    </div>
+  );
+};
+
+export default RestaurantMenuPage;
